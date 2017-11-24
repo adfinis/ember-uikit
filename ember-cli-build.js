eb6eee761c73c2a724777e1b3e3bb44b440c7f60
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-24 09:52:29 +0100

    Fix demo page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    vendorFiles: {
-      'jquery.js': null
-    },
     fingerprint: {
       prepend: 'https://anehx.github.io/ember-uikit/'
     },
