c4f39c2db73c736559c2093e7bcb1c6891742f58
Jay Varner <jayvarner@gmail.com>
2018-01-30 09:55:30 -0500

    Add shim to import Uikit as module.
    Fixed when shim is imported.

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ module.exports = {
 
     if (this.uikitOptions.importUIkitJS) {
       this.app.import(path.join('vendor', 'uikit.js'));
+      this.app.import(path.join('vendor', 'shims', 'uikit.js'));
 
       if (this.uikitOptions.useIcons) {
         this.app.import(path.join('vendor', 'uikit-icons.js'));
