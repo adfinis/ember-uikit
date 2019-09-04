641bdafa75fd6f634902fd9716a879554af1321e
stefanhofmann2 <stefan.hofmann@posteo.de>
2019-09-04 15:09:28 +0200

    feat(uk-switcher): add uikit events for the switcher component

--- addon/components/uk-switcher/content/item.js
+++ addon/components/uk-switcher/content/item.js
@@ -1,8 +1,31 @@
 import Component from "@ember/component";
 import layout from "../../../templates/components/uk-switcher/content/item";
+import UIkit from "uikit";
+
+// empty function as default event handler
+const noop = () => {};
 
 export default Component.extend({
   layout,
 
-  tagName: "li"
+  tagName: "li",
+
+  setEvents() {
+    let events = {
+      beforeshow: this.getWithDefault("on-beforeshow", noop),
+      show: this.getWithDefault("on-show", noop),
+      shown: this.getWithDefault("on-shown", noop),
+      beforehide: this.getWithDefault("on-beforehide", noop),
+      hide: this.getWithDefault("on-hide", noop),
+      hidden: this.getWithDefault("on-hidden", noop)
+    };
+
+    for (let event in events) {
+      UIkit.util.on(this.element, event, events[event]);
+    }
+  },
+  didInsertElement() {
+    this._super(...arguments);
+    this.setEvents();
+  }
 });
