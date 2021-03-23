dad8eda866a20d0b840fcfc890764b52837c3d16
Jonas Metzener <jonas.metzener@adfinis.com>
2021-03-23 17:30:51 +0100

    chore(docs): refactor links to bracket syntax

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     "no-action": "warn",
     "no-implicit-this": "warn",
     "no-curly-component-invocation": "warn",
-    "no-link-to-positional-params": "warn",
     "require-input-label": "warn",
     "no-obsolete-elements": "warn",
     "no-yield-only": "warn",
