fcddb14ed71d338e4849c880c34fd47516ffc1f0
Jonas Metzener <jonas.metzener@adfinis.com>
2022-02-04 11:06:55 +0100

    fix(config): remove usage of ember-get-config

--- addon/services/notification.js
+++ addon/services/notification.js
@@ -1,30 +1,32 @@
+import { getOwner } from "@ember/application";
 import Service from "@ember/service";
-import config from "ember-get-config";
 import UIkit from "uikit";
 
 function notification(status) {
   return function () {
     return {
       value(message, options = {}) {
-        return this._notification(message, Object.assign(options, { status }));
+        const config =
+          getOwner(this).resolveRegistration("config:environment")[
+            "ember-uikit"
+          ]?.notification ?? {};
+
+        const n = UIkit.notification({
+          ...config,
+          ...options,
+          status,
+          message,
+        });
+
+        return n?.$el
+          ? new Promise((resolve) => UIkit.util.on(n.$el, "close", resolve))
+          : Promise.resolve();
       },
     };
   };
 }
 
 export default class NotificationService extends Service {
-  _notification(message, options) {
-    const n = UIkit.notification(
-      Object.assign(config["ember-uikit"]?.notification ?? {}, options, {
-        message,
-      })
-    );
-
-    return n?.$el
-      ? new Promise((resolve) => UIkit.util.on(n.$el, "close", resolve))
-      : Promise.resolve();
-  }
-
   @notification("default") default;
   @notification("primary") primary;
   @notification("success") success;
