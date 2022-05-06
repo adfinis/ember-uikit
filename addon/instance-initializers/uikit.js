a3562b04fcc0691fd89dad33a0362297d954b9fd
Jonas Metzener <jonas.metzener@adfinis.com>
2022-05-06 13:19:42 +0200

    fix(engine): add engine compatibility for the global container

--- addon/instance-initializers/uikit.js
+++ addon/instance-initializers/uikit.js
@@ -1,7 +1,14 @@
+import { getOwner } from "@ember/application";
 import UIkit from "uikit";
 
 export function initialize(appInstance) {
-  UIkit.container = appInstance.rootElement;
+  // As there is only a single `Router` across the whole app, which is owned
+  // by the root `Application`, this reliably finds the root `Application`
+  // from an `Application` or `Engine`.
+  // eslint-disable-next-line ember/no-private-routing-service
+  const hostApp = getOwner(appInstance.lookup("router:main"));
+
+  UIkit.container = hostApp.rootElement;
 }
 
 export default { initialize };
