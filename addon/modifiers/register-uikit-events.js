54aaed0fd67c28b162608722f7ce1b3f6c7dcc89
Jonas Metzener <jonas.metzener@adfinis.com>
2022-09-12 16:49:24 +0200

    fix(modifiers): ensure modifier compatibility with the new modifier API

--- addon/modifiers/register-uikit-events.js
+++ addon/modifiers/register-uikit-events.js
@@ -2,12 +2,11 @@ import { run } from "@ember/runloop";
 import { modifier } from "ember-modifier";
 import UIkit from "uikit";
 
-export default modifier(function registerUikitEvents(
-  element,
-  positional,
-  named
-) {
-  Object.entries(named).forEach(([name, fn]) => {
-    UIkit.util.on(element, name, (...args) => run(() => fn(...args)));
-  });
-});
+export default modifier(
+  (element, positional, named) => {
+    Object.entries(named).forEach(([name, fn]) => {
+      UIkit.util.on(element, name, (...args) => run(() => fn(...args)));
+    });
+  },
+  { eager: false }
+);
--- addon/modifiers/uk-tooltip.js
+++ addon/modifiers/uk-tooltip.js
@@ -1,8 +1,11 @@
 import { modifier } from "ember-modifier";
 import UIkit from "uikit";
 
-export default modifier(function ukTooltip(element, [title], options) {
-  const tooltip = UIkit.tooltip(element, { title, ...options });
+export default modifier(
+  (element, [title], options) => {
+    const tooltip = UIkit.tooltip(element, { title, ...options });
 
-  return () => tooltip.$destroy();
-});
+    return () => tooltip.$destroy();
+  },
+  { eager: false }
+);
