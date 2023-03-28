9b3cff9997471dd47e77470b2065c5b08b6a72b9
Jonas Metzener <jonas.metzener@adfinis.com>
2023-03-28 16:46:16 +0200

    chore(ci): fix codecov action

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,10 @@
 import { setApplication } from "@ember/test-helpers";
 import Application from "dummy/app";
 import config from "dummy/config/environment";
+import {
+  forceModulesToBeLoaded,
+  sendCoverage,
+} from "ember-cli-code-coverage/test-support";
 import { start } from "ember-qunit";
 import * as QUnit from "qunit";
 import { setup } from "qunit-dom";
@@ -10,3 +14,8 @@ setApplication(Application.create(config.APP));
 setup(QUnit.assert);
 
 start();
+
+QUnit.done(async function () {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+});
